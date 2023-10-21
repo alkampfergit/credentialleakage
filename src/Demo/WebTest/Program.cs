785d9c7ac238ddeba111b72fd5b02e97e8b8d295
Gian Maria Ricci <alkampfer@nablasoft.com>
2023-10-21 15:19:23 +0200

    sto facendo la feature foo

--- src/Demo/WebTest/Program.cs
+++ src/Demo/WebTest/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+///FEATURE FOO
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
